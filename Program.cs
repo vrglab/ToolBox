6406e6bd37713adaf8895b268b4b787c05c42cf5
vrglab <vrglab1@gmail.com>
2023-03-08 14:42:08 +0100

    mysql

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ToolBox.ToolBox.DataBase;
 
 namespace ToolBox
 {
@@ -16,6 +17,7 @@ namespace ToolBox
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Database.OpenConnection();
             Application.Run(new Main());
         }
     }
--- ToolBox/DataBase/Database.cs
+++ ToolBox/DataBase/Database.cs
@@ -6,15 +6,41 @@ using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 using System.Runtime.InteropServices;
+using ICSharpCode.Decompiler.IL;
+using System.IO;
 
 namespace ToolBox.ToolBox.DataBase
 {
     public class Database
     {
+        static MySqlConnection mconect;
         public Database() 
         { 
-            MySqlConnection mconect = new MySqlConnection("server=localhost;user=root;database=cm4_5;");
+            
+        }
+
+        public static void OpenConnection()
+        {
+            mconect = new MySqlConnection("server=localhost;user=root;database=cm4_5;");
             mconect.Open();
+        }
+
+        public static object[] Querry(string querry)
+        {
+            MySqlCommand cmd = new MySqlCommand(querry, mconect);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+
+
+
+
+            List<object> data = new List<object>();
+
+            for (int i = 0; i < rdr.FieldCount; i++)
+            {
+                data.Add(rdr.GetValue(i));
+            }
+
+            return data.ToArray();
         }
     }
 }
