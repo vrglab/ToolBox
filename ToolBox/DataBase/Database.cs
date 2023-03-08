d00e450de5b353801182e144301c227504ce22ca
vrglab <vrglab1@gmail.com>
2023-03-08 14:33:20 +0100

    database

---
+++ ToolBox/DataBase/Database.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Runtime.InteropServices;
+
+namespace ToolBox.ToolBox.DataBase
+{
+    public class Database
+    {
+        public Database() 
+        { 
+            MySqlConnection mconect = new MySqlConnection("server=localhost;user=root;database=cm4_5;");
+            mconect.Open();
+        }
+    }
+}
